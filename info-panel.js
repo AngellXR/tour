b1cc6985e073e34f6ef57ac414092f88392bc99d
Jesse <63426722+jsealt@users.noreply.github.com>
2020-12-29 21:50:18 -0500

    not bad for a day
    tweaked components, may need to remove rounding of the images being served as there is a silly white corner on the components.
    
    Need to reanimate the gifs inside the cards
    
    need to swap out skybox with relevant 360 image on hover of card.
    
    Need to link from card to experience. (this is all in trello as well, obviously :+1: )

--- info-panel.js
+++ info-panel.js
@@ -10,19 +10,19 @@ AFRAME.registerComponent('info-panel', {
   
       this.movieInfo = {
         karigurashiButton: {
-          title: 'The Secret World of Arrietty (2010)',
+          title: 'SBY Aerial VR Tour (cloudpano)',
           imgEl: document.querySelector('#karigurashiMovieImage'),
-          description: 'Based on the 1952 novel The Borrowers by Mary Norton, an English author of children\'s books, about a family of tiny people who live secretly in the walls and floors of a typical household, borrowing items from humans to survive.'
+          description: 'A beautiful SBY aerial Tour in Full HD.'
         },
         kazetachinuButton: {
-          title: 'The Wind Rises (2013)',
+          title: 'Uncle Jons Soap Shop (Matterport)',
           imgEl: document.querySelector('#kazetachinuMovieImage'),
-          description: 'The Wind Rises is a fictionalised biographical film of Jiro Horikoshi (1903, 1982), designer of the Mitsubishi A5M fighter aircraft and its successor, the Mitsubishi A6M Zero, used by the Empire of Japan during World War II. The film is adapted from Miyazaki\'s manga of the same name, which was in turn loosely based on both the 1937 novel The Wind Has Risen by Tatsuo Hori and the life of Jiro Horikoshi.'
+          description: 'Mission: To provide healthy, handmade products at affordable prices and no hype!'
         },
         ponyoButton: {
-          title: 'Ponyo (2003)',
+          title: 'Lilac CCo. (insta360, gothru)',
           imgEl: document.querySelector('#ponyoMovieImage'),
-          description: 'It is the eighth film Miyazaki directed for Studio Ghibli, and his tenth overall. The film tells the story of Ponyo (Nara), a goldfish who escapes from the ocean and is rescued by a five-year-old human boy, Sōsuke (Doi) after she is washed ashore while trapped in a glass jar.'
+          description: 'Step inside a clothing boutique specializing in Juniors and Womens clothing and accessories. We retail clothing for all body types.'
         }
       };
   
